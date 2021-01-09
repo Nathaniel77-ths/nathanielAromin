41a992c69f0c3f7f41aa07d8eac93b8e932a67b5
Nathaniel77-ths <76142189+Nathaniel77-ths@users.noreply.github.com>
2021-01-09 23:58:27 +0800

    update_1_9_2021

---
+++ UC3LO2/addToList.js
@@ -0,0 +1,9 @@
+function addList()
+{
+
+var newDiner = document.getElementById("name").value;
+const addDiner = document.querySelector("#dinerTable");
+
+addDiner.innerHTML += "<li>" + newDiner +"</li>";
+}
+
